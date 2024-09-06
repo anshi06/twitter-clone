c1c7df8ec798c58a359c9aeb0db462dec4a1f6af
Anshika Upadhyay <anshika@kindtech.co.in>
2024-09-06 22:03:22 +0530

    wrong-commit-2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./components/Signup/Login";
 import Profile from "./components/ProfileSection/Profile";
 
 function App() {
-  console.log('Branch-2-copy')
+  console.log('Branch-2-copy-copy')
   return (
     <div>
       <BrowserRouter>
